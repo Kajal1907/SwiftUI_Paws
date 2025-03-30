e27edcd324ed5d47f2e4e62d8ac5f1896dcd1ab3
Kajal <kajal.singh@olx.in>
2025-03-30 17:05:30 +0530

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Kajal on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
